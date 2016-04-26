ed8bac0ada1f847c5549c4fa9bcf040c3894ec91
Igor Lapin <raynor73@gmail.com>
2016-04-26 23:10:41 +0300

    Sync

--- app/src/main/java/com/example/simplefacebookclient/MainActivity.java
+++ app/src/main/java/com/example/simplefacebookclient/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.simplefacebookclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	public void onTestButtonClick(final View view) {
+		startActivity(new Intent(this, com.facebook.FacebookActivity.class));
 	}
 }
